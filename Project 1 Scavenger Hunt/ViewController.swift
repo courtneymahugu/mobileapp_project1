1b008790378e530a71902ebf362b0e7563f5d45b
courtneymahugu <courtneymahugu@gmail.com>
2025-03-09 16:47:36 -0400

    Initial Commit

---
+++ Project 1 Scavenger Hunt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 1 Scavenger Hunt
+//
+//  Created by Courtney Mahugu on 3/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
